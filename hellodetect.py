31cc5de18417f82ccaff28910dab7426d4522577
moahmed abdelwahab <moh.elwah@gmail.com>
2023-01-21 20:39:40 +0000

    change

--- hellodetect.py
+++ hellodetect.py
@@ -4,4 +4,5 @@ def detect(bucket, name):
     #rekognition = boto3.client('rekognition')
     print(f'this is bucket {bucket}, this is name {name}')
 
+file = 's3://film-studio-computer-vision-2023/lebron-lakers.jpg'
 detect('foo', 'boo')
